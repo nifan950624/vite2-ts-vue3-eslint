59b4357a0aa028f8bf20564c33ddf010ea5c4caa
v_vyqffni <v_vyqffni@tencent.com>
2022-08-03 11:26:40 +0800

    feat: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory, Router } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', component: () => import('@/views/Home/index.vue') },
-  { path: '/detail', component: () => import('@/views/Detail/index.vue') },
 ];
 
 const router: Router = createRouter({
